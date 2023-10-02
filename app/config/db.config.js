2398ce765acbc87e81a4869eb72f90f30bc7fccd
Suwara Songthong <s6330613008@phuket.psu.ac.th>
2023-10-02 16:42:29 +0700

    first commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "myapi"
+    HOST: "bt7gbsyityqdq7e28wu4-mysql.services.clever-cloud.com",
+    USER: "uqim1mh7n4yqzulp",
+    PASSWORD: "QanYDRz75ACTjD5PYAme",
+    DB: "bt7gbsyityqdq7e28wu4"
 };
--- app/models/db.js
+++ app/models/db.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql2");
-// const dbConfig = require("../config/db.config");  //           ../ it mean go to the folder
+const dbConfig = require("../config/db.config");  //           ../ it mean go to the folder
 
-// const connection = mysql.createConnection({
-//     host: dbConfig.HOST,
-//     user: dbConfig.USER,
-//     password: dbConfig.PASSWORD,
-//     database: dbConfig.DB
-// });
+const connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+});
 
 //for PlanetScale
-require('dotenv').config()
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+// require('dotenv').config()
+// const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 connection.connect( (error) => {
     if(error) console.log("MySQL Connection: "+error);
